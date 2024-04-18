e72e8a28147a42a1f12b0215684d6d4721017f0e
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-04-17 23:07:30 -0600

    endpoint for carouselpopulate working, full loading from db into frontend

--- SQL/inserttodb/insert.py
+++ SQL/inserttodb/insert.py
@@ -2,13 +2,15 @@
 
 
 import os
+import hashlib
 from dotenv import load_dotenv
-import random, string
-import mysql.connector
-
 # loads environment variables
 load_dotenv()
-
+import random, string
+import mysql.connector
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import requests
 mysql_password = os.getenv('MYSQL_PASSWORD')
 mysql_db = os.getenv('MYSQL_DATABASE')
 mysql_host = os.getenv('MYSQL_HOST')
@@ -23,9 +25,6 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
-
-
-
 
 # randomly generate:  album ratings/5, song ratings/5, and calculate avg rating (will need to do in sql later)
 def random_word(length):
@@ -46,60 +45,107 @@ def get_random_song_ids(length):
     ids.append(randRating)
   return ids
 
-# able to be manually input
-artist_name = input("Artist name")
-artist_id = random.getrandbits(128)
-album_title = input("Album title")
-album_id = random.getrandbits(128)
-album_ratings = get_random_ratings(10)
-date_released = input("Date released (MM_DD_YY)")
-song_titles = input("Song titles (separated by commas)")
-song_ids = get_random_song_ids(10)
-song_ratings = get_random_ratings(10)
-artist_id = random.getrandbits(128)
-artist_bio = random_word(256)
+def hash_string(string):
+  encoded_string = string.encode()
+  hasher = hashlib.sha256()
+  hasher.update(encoded_string)
+  return hasher.hexdigest()
 
-# calculate avg album rating
-album_avg_rating = 0
-total = 0
-for i in album_ratings:
-  total += i
-album_avg_rating = total / len(album_ratings)
 
-# calculate avg song rating
-song_avg_rating = 0
-total = 0
-for i in song_ratings:
-  total += i
-song_avg_rating = total / len(album_ratings)
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+auth_response = requests.post(AUTH_URL, {
+  'grant_type': 'client_credentials',
+  'client_id': os.getenv("SPOTIPY_CLIENT_ID"),
+  'client_secret': os.getenv("SPOTIPY_CLIENT_SECRET")
+})
+auth_response_data = auth_response.json()
+access_token = auth_response_data['access_token']
+print(access_token)
 
-sql = "INSERT INTO artists (artist_name, artist_id, bio) VALUES (%s, %s, %s)"
-val = [
-  (artist_name, artist_id, random_word(256))
-]
-mycursor.executemany(sql, val)
 
-sql = "INSERT INTO albums (album_title, album_id, date_released, artist_id, avg_rating) VALUES (%s, %s, %s, %s, %s)"
-val = [
-  (album_title, album_id, date_released, artist_id, album_avg_rating)
-]
-mycursor.executemany(sql, val)
+BASE_URL = 'https://api.spotify.com/v1/'
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
 
-sql = "INSERT INTO album_artists (artist_id, album_id) VALUES (%s, %s)"
-val = [
-  (artist_id, album_id)
-]
-mycursor.executemany(sql, val)
+# my kitchen sync playlist id
+playlist_id = '2ZqjemHmfmmpVomtt85Vd3'
 
-# map list of songs to vals to send to db
-list_of_songs = song_titles.split(",")
-print(list_of_songs)
+for x in range(3, 27):
+  
+  r = requests.get(BASE_URL + f'playlists/{playlist_id}/tracks?offset={x*100}', headers=headers)
+  r = r.json()
+  print(len(r['items']))
 
-val = []
-for (i, song_title_) in enumerate(list_of_songs):
-  val.append((song_title_, random.getrandbits(3), artist_id, date_released, album_id, random.getrandbits(128), song_avg_rating))
+  for i in range(len(r['items'])):
+    # gets release date of album
+    date_released = r['items'][i]['track']['album']['release_date']
+    
+    # gets name of album of track
+    album_title = r['items'][i]['track']['album']['name']
 
-sql = "INSERT INTO songs (song_title, duration, artist_id, date_released, album_id, song_id, avg_rating) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-mycursor.executemany(sql, val)
+    # gets name of title of track
+    song_title = r['items'][i]['track']['name']
 
-mydb.commit()
+    # gets name of artist
+    artist_name = r['items'][i]['track']['album']['artists'][0]['name']
+
+    # able to be manually input
+    #artist_name = input("Artist name: ")
+    artist_id = hash_string(artist_name)
+    #album_title = input("Album title: ")
+    album_id = hash_string(album_title)
+    album_ratings = get_random_ratings(10)
+    # date_released = input("Date released (MM_DD_YY): ")
+    # song_titles = input("Song titles (separated by commas): ")
+    # song_ids = get_random_song_ids(10)
+    song_ratings = get_random_ratings(10)
+    artist_bio = hash_string(artist_name + artist_id)
+
+    # calculate avg album rating
+    album_avg_rating = 0
+    total = 0
+    for i in album_ratings:
+      total += i
+    album_avg_rating = total / len(album_ratings)
+
+    # calculate avg song rating
+    song_avg_rating = 0
+    total = 0
+    for i in song_ratings:
+      total += i
+    song_avg_rating = total / len(album_ratings)
+
+    sql = "INSERT IGNORE INTO artists (artist_name, artist_id, bio) VALUES (%s, %s, %s)"
+    val = [
+      (artist_name, artist_id, artist_bio)
+    ]
+    mycursor.executemany(sql, val)
+
+    sql = "INSERT IGNORE INTO albums (album_title, album_id, date_released, artist_id, avg_rating) VALUES (%s, %s, %s, %s, %s)"
+    val = [
+      (album_title, album_id, date_released, artist_id, album_avg_rating)
+    ]
+    mycursor.executemany(sql, val)
+
+    sql = "INSERT IGNORE INTO album_artists (artist_id, album_id) VALUES (%s, %s)"
+    val = [
+      (artist_id, album_id)
+    ]
+    mycursor.executemany(sql, val)
+
+    # map list of songs to vals to send to db
+    # list_of_songs = song_titles.split(",")
+    # print(list_of_songs)
+
+    # very cool but unfortunately i don't need it right now
+    # val = []
+    # for (i, song_title_) in enumerate(list_of_songs):
+    #   val.append((song_title_, random.getrandbits(3), artist_id, date_released, album_id, random.getrandbits(128), song_avg_rating))
+
+    sql = "INSERT IGNORE INTO songs (song_title, duration, artist_id, date_released, album_id, song_id, avg_rating) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    val = []
+    val.append((song_title, random.getrandbits(3), artist_id, date_released, album_id, random.getrandbits(128), song_avg_rating))
+    mycursor.executemany(sql, val)
+
+    mydb.commit()
