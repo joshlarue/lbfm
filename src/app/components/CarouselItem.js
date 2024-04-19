07ccd15e9baaf968c4703c9c6e887975549cb868
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-04-19 11:03:29 -0600

    using album image links from scdn.co (spotify cdn) to keep db small

--- src/app/components/CarouselItem.js
+++ src/app/components/CarouselItem.js
@@ -6,7 +6,7 @@ export default function CarouselItem(props) {
     <>
       <Link href={'/albums/' + props.id} className="flex flex-col w-fit p-5">
         <div className="w-[15vh] min-h-[15vh] bg-base rounded-lg relative">
-          <Image src={props.albumImage} className="rounded-lg shadow-md object-cover" fill='true' alt={props.alt}/>
+          <Image src={props.albumImage} className="rounded-lg shadow-md object-cover" fill='true' alt={"Album cover: "+props.alt}/>
         </div>
         <div className="flex justify-between py-2 w-[15vh]">
           <div className="flex flex-col w-full gap-1 min-h-fit justify-between">
