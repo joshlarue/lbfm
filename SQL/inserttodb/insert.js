3ab897443b9bab96e3f8d294df69d4dcb09fe1ab
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-05-18 18:06:37 -0600

    updating insert script with js instead of python

---
+++ SQL/inserttodb/insert.js
@@ -0,0 +1,139 @@
+import pool from "../../src/app/db/pool.js";
+import sha256 from "sha256";
+
+const AUTH_URL = "https://accounts.spotify.com/api/token";
+const API_URL = "https://api.spotify.com/v1";
+
+// acquire access token from spotify
+const authResponse = await fetch(AUTH_URL, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+  body: `grant_type=client_credentials&client_id=${process.env.SPOTIFY_CLIENT_ID}&client_secret=${process.env.SPOTIFY_CLIENT_SECRET}`,
+});
+const authResponseJson = await authResponse.json();
+const spotifyAccessToken = authResponseJson["access_token"];
+
+const connection = pool.getConnection();
+const playlistId = "2ZqjemHmfmmpVomtt85Vd3";
+
+for (let i = 0; i < 100; i++) {
+  const playlistResponse = await fetch(
+    // fetch 50 tracks from the playlist at a time
+    `${API_URL}/playlists/${playlistId}/tracks?offset=${(i + 1) * 50}&limit=50`
+  );
+  for (let j = 0; i < playlistResponse["items"].length; i++) {
+    const dbAlbumId = sha256(
+      playlistResponse["items"][j]["track"]["album"]["name"]
+    );
+    const dbArtistId = sha256(
+      playlistResponse["items"][j]["track"]["album"]["artists"][0]["name"]
+    );
+
+    const albumExistsResponse = connection.query(`
+      SELECT album_id FROM albums
+      WHERE album_id = ${dbAlbumId};
+    `);
+    const artistExistsResponse = connection.query(`
+      SELECT artist_id FROM artists
+      WHERE artist_id = ${dbArtistId};
+    `);
+
+    let [insertAlbum, insertArtist] = [false, false];
+    if (albumExistsResponse[0].length > 0) insertAlbum = true;
+    if (artistExistsResponse[0].length > 0) insertArtist = true;
+
+    if (insertAlbum || insertArtist) {
+      const spotifyAlbumId = playlistResponse["items"][i]["track"]["album"][
+        "href"
+      ]
+        .split("https://open.spotify.com/album/")[1]
+        .split("?")[0];
+
+      getAlbumAndInsert(spotifyAlbumId, insertAlbum, insertArtist);
+    }
+  }
+}
+
+async function getAlbumAndInsert(spotifyAlbumId, insertArtist, insertAlbum) {
+  // fetch album info from spotify
+  const albumResponse = await fetch(`${API_URL}/albums/${spotifyAlbumId}`, {
+    headers: new Headers({
+      Authorization: `Bearer ${spotifyAccessToken}`,
+    }),
+  });
+  const albumResponseJson = await albumResponse.json();
+
+  // use album info to populate the following fields for insertion
+  const releaseDate = albumResponseJson["release_date"];
+  const albumTitle = albumResponseJson["name"].replace(/'/gi, "''");
+  const albumId = sha256(albumTitle);
+  const artistName = albumResponseJson["artists"][0]["name"].replace(
+    /'/gi,
+    "''"
+  );
+  const artistId = sha256(artistName);
+  const albumImg = albumResponseJson["images"][0]["url"];
+  const albumTracks = albumResponseJson["tracks"]["items"];
+  const randomAvgRating = Math.floor(Math.random() * 5) + 1;
+
+  try {
+    if (insertArtist) {
+      // insert artist info if they do not already exist in the DB
+      (await connection).execute(`
+      INSERT INTO artists
+      (artist_name, artist_id)
+      VALUES ('${artistName}', '${artistId}');
+    `);
+      console.log("Successfully inserted artist");
+    }
+
+    if (insertAlbum) {
+      // insert album info if album does not exist
+      (await connection).execute(`
+      INSERT INTO albums
+      (album_title, date_released, album_id, album_image, artist_id, avg_rating)
+      VALUES ('${albumTitle}', '${releaseDate}', '${albumId}', '${albumImg}', '${artistId}', ${randomAvgRating});
+    `);
+
+      // insert info into album_artist bridging table if info does not already exist
+      (await connection).execute(`
+      INSERT INTO album_artists
+      (artist_id, album_id)
+      VALUES ('${artistId}', '${albumId}');
+    `);
+
+      // this loop checks to ensure track numbers are not doubled up in the case of dual/triple disc albums e.g. Mr. Morale on Spotify
+      // after track numbers are checked/fixed for doubles, curSong is inserted if it does not already exist in the DB
+      let trackNums = [];
+      for (let i = 0; i < albumTracks.length; i++) {
+        const curSong = albumTracks[i];
+
+        // replaces single quotes with two single quotes ('') to escape them for the DB
+        const songId = sha256(curSong["name"].replace(/'/gi, "''"));
+
+        let trackNumber = curSong["track_number"];
+        for (let j = 0; j < trackNums.length; j++) {
+          if (trackNums[j] === trackNumber) {
+            trackNumber = trackNums[j] + 1;
+          }
+        }
+        trackNums.push(trackNumber);
+
+        // insert song into db if songs do not exist
+        (await connection).execute(`
+        INSERT IGNORE INTO songs
+        (song_title, track_number, artist_id, album_id, song_id)
+        VALUES ('${curSong["name"].replace(
+          /'/gi,
+          "''"
+        )}', '${trackNumber}', '${artistId}', '${albumId}', '${songId}');
+      `);
+      }
+      console.log("Successfully inserted album_artists and songs!");
+    }
+  } catch {
+    console.error("Massive error! Good thing I 'caught' it!");
+  }
+}
